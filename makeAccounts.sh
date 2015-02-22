f620fcd15a08be5f03680667efe5667e07b5ec34
Sudarshan Muralidhar <sudarshan.muralidhar@gmail.com>
2015-02-22 01:40:20 -0500

    modify script to run only 5 jobs at a time

--- makeAccounts.sh
+++ makeAccounts.sh
@@ -1,7 +1,13 @@
 # makeAccounts.sh <number of accouts>
 
+function max5 {
+   while [ `jobs | wc -l` -ge 5 ]
+   do
+      sleep 2
+   done
+}
 for i in `seq 1 $1`;
 do
-  node app&
+  max5; node app &
 done 
 wait
